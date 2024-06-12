9e0215ee3850b5650886b6f044f2eb8e13fd87d0
Fiona Mellink <fimellink@gmail.com>
2024-06-13 08:54:33 +1000

    Complete lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,19 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        users_list = []
+        users = json.loads(self.get_response_body())
+        for user in users:
+            users_list.append(user)
+        return users_list
+
+get_requester = GetRequester("https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json")
+
+output = get_requester.load_json()
+
+for i in output:
+    print(f"{i['name']} is a {i['occupation']}")
